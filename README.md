5273fb6a1b26843747d1312da94a880a800bfcc0
nate.stuart <nate.stuart@gmail.com>
2017-05-07 15:01:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# mqtt-openzwave-bridge
+### Simple node.js app to expose the openzwave-shared library to mqtt
+
+## Usage
+Intended for use within a docker, uses environment variables to define several configuration options required for zwave library to know how to work in your environment.
+#### Environment Variables
+* __MQTT_HOST__ Your mqtt host with full URI, example "mqtt://myawesomemqttserver.com" (you'll probably want a fast local host due to the message rates the zwave controller can generate
+* __MQTT_USER__(optional) mqtt user
+* __MQTT_PASS__(optional) mqtt password
+* __MQTT_ZWAVE_TOPIC__ mqtt topic you want to publish your zwave activity to. Remember when running in a docker you will need to pass your actual device through as well.
+* __ZWAVE_DEVICE__ port/location the zwave stick is on. Examples for Windows: "\\\\.\\COM3", Linux:"/dev/ttyUSB0", Raspberry Pi:"/dev/ttyACM0"
+
+#### Additional Configuration
+The app will look for a OpenZwave.json configuration file in the __/zwave__ folder for additional configuration options, in addition to storing the openzwave logs there. A full list of configuration options can be found here: https://github.com/OpenZWave/open-zwave/wiki/Config-Options
---
+++ mqtt-openzwave-bridge.js
@@ -0,0 +1,374 @@
+
+const logging = require('./lib/logging.js')
+const util = require('util')
+const OpenZwave = require('openzwave-shared')
+const mqtt = require('mqtt')
+const fs = require('fs');
+
+var ozwConfig = {};
+var ozw = null;
+var ozwConnected = false;
+var driverReadyStatus = false;
+var allowunreadyupdates = true;
+var mqttConnected = false;
+var basedir = "/zwave/"
+var configFile = basedir + "OpenZwave.json"
+var UUIDPREFIX = "_macaddr_";
+var HOMENAME = "_homename_";
+
+var nodes = {};
+
+logging.set_enabled(true);
+
+var ozwEvents = {
+    'driver ready': driverReady,
+    'driver failed': driverFailed,
+    'node added': nodeAdded,
+    'node ready': nodeReady,
+    'node event': nodeEvent,
+    'value added': valueAdded,
+    'value changed': valueChanged,
+    'value removed': valueRemoved,
+    'notification': notification,
+    'scan complete': scanComplete,
+    'controller command': controllerCommand
+};
+
+// Config
+const mqttHost = process.env.MQTT_HOST
+const mqttUsername = process.env.MQTT_USER
+const mqttPassword = process.env.MQTT_PASS
+const zwaveTopic = process.env.MQTT_ZWAVE_TOPIC
+const zwaveDevice = process.env.ZWAVE_DEVICE
+
+var openZwaveOptions = {};
+
+if (fs.existsSync(configFile)) {
+    openZwaveOptions = JSON.parse(fs.readFileSync(configFile, "utf8"))
+}
+
+
+// Setup MQTT
+var mqttOptions = {}
+if (mqttUsername !== null && mqttUsername !== undefined)
+    mqttOptions.username = mqttUsername
+if (mqttPassword !== null && mqttPassword !== undefined)
+    mqttOptions.password = mqttPassword
+
+client = mqtt.connect(mqttHost, mqttOptions)
+
+
+// MQTT Observation
+client.on('error', (error) => {
+    logging.log("mqtt error: " + error)
+})
+
+client.on('connect', () => {
+    logging.log('mqtt connected')
+    mqttConnected = true;
+    client.subscribe(zwaveTopic + '/set/#')
+
+    ///Wait for mqtt connection before we fire up the zwave controller the first time to ensure we capture all activity
+    if (!ozwConnected) {
+
+        ozw = new OpenZwave(openZwaveOptions);
+
+        //Map the callbacks to zwave events
+        Object.keys(ozwEvents).forEach(function (evt) {
+            logging.log('addListener ' + evt);
+            ozw.on(evt, ozwEvents[evt]);
+        });
+
+        /* time to connect to zwave stick */
+        logging.log('connecting to ' + zwaveDevice);
+        ozw.connect(zwaveDevice);
+        ozwConnected = true;
+
+    }
+})
+
+//I thought you loved me!?
+client.on('disconnect', () => {
+    logging.log('mqtt reconnecting...\n')
+    client.connect(mqttHost, mqttOptions) //You'll come around
+})
+
+
+//Parse the incomming mqtt message for some basic actions, or delve into the full API
+client.on('message', (topic, message) => {
+    try {
+        //Trim (crudely) our zwave main topic
+        topic = topic.substring(zwaveTopic.length + 5);
+
+        logging.log(' mqtt message received, topic:' + topic + ', message: ' + message)
+
+        var payload;
+
+        try {
+            //We're expecting all messages to be in a JSON format
+            payload = JSON.parse(message);
+        } catch (err) {
+            logging.warn('Illegal message! Msg:"' + message + '"  Error:(' + err + ')');
+            return;
+        }
+
+        logging.log(JSON.stringify(payload, null, 2));
+
+        switch (true) {
+            // switch On/Off: for basic single-instance switches and dimmers
+            case /switchOn/.test(topic):
+                ozw.setValue(payload.nodeid, 37, 1, 0, true);
+                break;
+            case /switchOff/.test(topic):
+                ozw.setValue(payload.nodeid, 37, 1, 0, false);
+                break;
+
+            // setLevel: for dimmers
+            case /setLevel/.test(topic):
+                ozw.setValue(payload.nodeid, 38, 1, 0, payload.value);
+                break;
+
+            // setValue: for everything else
+            case /setValue/.test(topic):
+                logging.log(util.format("ZWaveOut.setValue payload: %j", payload));
+                ozw.setValue(
+                    payload.nodeid, (payload.cmdclass || 37), // default cmdclass: on-off
+                    (payload.instance || 1), // default instance
+                    (payload.cmdidx || 0), // default cmd index
+                    payload.value
+                );
+                break;
+
+            /* EXPERIMENTAL: send basically every available command down
+             * to OpenZWave, just name the function in the message topic
+             * and pass in the arguments as "payload.args" as an array:
+             * {"topic": "someOpenZWaveCommand", "payload": {"args": [1, 2, 3]}}
+             * If the command needs the HomeID as the 1st arg, use "payload.prependHomeId"
+             * */
+            default:
+                if (topic && typeof ozw[topic] === 'function' && payload) {
+
+                    var args = payload.args || [];
+
+                    if (payload.prependHomeId) args.unshift(ozwConfig.homeid);
+
+                    logging.log('attempting direct API call to ' + topic + '()');
+
+                    try {
+                        var result = ozw[topic].apply(ozw, args);
+                        logging.log('direct API call success, result=' + JSON.stringify(result));
+                        if (typeof result != 'undefined') {
+                            payload.result = result;
+                            // send off the direct API call's result to the output
+                            client.publish(zwaveTopic + '/apiResult/' + topic, JSON.stringify(payload));
+                        }
+                    } catch (err) {
+                        logging.log('direct API call to ' + topic + ' failed: ' + err, 'error');
+                    }
+                }
+        }
+    }
+    catch (ex) {
+        logging.warn('zwave error (' + ex + ')');
+        zwcallback("zwave error", ex);
+        return
+    }
+})
+
+//Zwave driver ready
+function driverReady(homeid) {
+    driverReadyStatus = true;
+    ozwConfig.homeid = homeid;
+    var homeHex = '0x' + homeid.toString(16);
+    HOMENAME = homeHex;
+    ozwConfig.name = homeHex;
+    logging.log('scanning network with homeid: ' + homeHex);
+    zwcallback('driver ready', ozwConfig);
+}
+
+//Zwave driver went badly
+function driverFailed() {
+    zwcallback('driver failed', ozwConfig);
+    process.exit();
+}
+
+//New node seen on network for this session
+function nodeAdded(nodeid) {
+    nodes[nodeid] = {
+        manufacturer: '',
+        manufacturerid: '',
+        product: '',
+        producttype: '',
+        productid: '',
+        type: '',
+        name: '',
+        loc: '',
+        classes: {},
+        ready: false,
+    };
+    zwcallback('node added', {
+        "nodeid": nodeid
+    });
+}
+
+//New node value found
+function valueAdded(nodeid, comclass, valueId) {
+    var ozwnode = nodes[nodeid];
+    if (!ozwnode) {
+        logging.log('valueAdded: no such node: ' + nodeid + ' error!');
+    }
+    if (!ozwnode['classes'][comclass])
+        ozwnode['classes'][comclass] = {};
+    if (!ozwnode['classes'][comclass][valueId.instance])
+        ozwnode['classes'][comclass][valueId.instance] = {};
+    // add to cache
+    logging.log("valueAdded: " + JSON.stringify(valueId));
+    ozwnode['classes'][comclass][valueId.instance][valueId.index] = valueId;
+    // tell NR
+    zwcallback('value added', {
+        "nodeid": nodeid,
+        "cmdclass": comclass,
+        "instance": valueId.instance,
+        "cmdidx": valueId.index,
+        "currState": valueId['value'],
+        "label": valueId['label'],
+        "units": valueId['units'],
+        "value": valueId
+    });
+}
+
+function valueChanged(nodeid, comclass, valueId) {
+    var ozwnode = nodes[nodeid];
+    if (!ozwnode) {
+        logging.log('valueChanged: no such node: ' + nodeid, 'error');
+    } else {
+        // valueId: OpenZWave ValueID (struct) - not just a boolean
+        var oldst;
+        if (ozwnode.ready || allowunreadyupdates) {
+            oldst = ozwnode['classes'][comclass][valueId.instance][valueId.index].value;
+            logging.log(util.format(
+                'zwave node %d: changed: %d:%s:%s -> %j', nodeid, comclass,
+                valueId['label'], oldst, JSON.stringify(valueId)));
+            // tell NR only if the node is marked as ready
+            zwcallback('value changed', {
+                "nodeid": nodeid,
+                "cmdclass": comclass,
+                "cmdidx": valueId.index,
+                "instance": valueId.instance,
+                "oldState": oldst,
+                "currState": valueId['value'],
+                "label": valueId['label'],
+                "units": valueId['units'],
+                "value": valueId
+            });
+        }
+        // update cache
+        ozwnode['classes'][comclass][valueId.instance][valueId.index] = valueId;
+    }
+}
+
+//aaaaand it's gone
+function valueRemoved(nodeid, comclass, instance, index) {
+    var ozwnode = nodes[nodeid];
+    if (ozwnode &&
+        ozwnode['classes'] &&
+        ozwnode['classes'][comclass] &&
+        ozwnode['classes'][comclass][instance] &&
+        ozwnode['classes'][comclass][instance][index]) {
+        delete ozwnode['classes'][comclass][instance][index];
+        zwcallback('value deleted', {
+            "nodeid": nodeid,
+            "cmdclass": comclass,
+            "cmdidx": index,
+            "instance": instance
+        });
+    } else {
+        logging.log('valueRemoved: no such node: ' + nodeid, 'error');
+    }
+}
+
+//Completed the scanning of this node, we're good to do what we want with it now
+function nodeReady(nodeid, nodeinfo) {
+    var ozwnode = nodes[nodeid];
+    if (ozwnode) {
+        for (var attrname in nodeinfo) {
+            if (nodeinfo.hasOwnProperty(attrname)) {
+                ozwnode[attrname] = nodeinfo[attrname];
+            }
+        }
+        ozwnode.ready = true;
+        //
+        logging.log('nodeReady: only|R|W| (nodeid-cmdclass-instance-index): type : current state');
+        for (var comclass in ozwnode['classes']) {
+            switch (comclass) {
+                case 0x25: // COMMAND_CLASS_SWITCH_BINARY
+                case 0x26: // COMMAND_CLASS_SWITCH_MULTILEVEL
+                case 0x30: // COMMAND_CLASS_SENSOR_BINARY
+                case 0x31: // COMMAND_CLASS_SENSOR_MULTILEVEL
+                case 0x60: // COMMAND_CLASS_MULTI_INSTANCE
+                    log.logging("         => Enabling polling for node " + nodeid + ":" + comclass);
+                    ozwDriver.enablePoll(nodeid, comclass);
+                    break;
+            }
+            var values = ozwnode['classes'][comclass];
+
+            for (var inst in values)
+                for (var idx in values[inst]) {
+                    var ozwval = values[inst][idx];
+                    var rdonly = ozwval.read_only ? '*' : ' ';
+                    var wronly = ozwval.write_only ? '*' : ' ';
+                    logging.log('           =>' + util.format(
+                        '\t|%s|%s| %s: %s:\t%s\t', rdonly, wronly, ozwval.value_id, ozwval.label, ozwval.value));
+                }
+        }
+        
+        zwcallback('node ready', {
+            nodeid: nodeid,
+            nodeinfo: nodeinfo
+        });
+    }
+}
+
+//Something evented
+function nodeEvent(nodeid, evtcode) {
+    logging.log('nodeEvent: ' + util.format('node %d: %d', nodeid, evtcode));
+    zwcallback('node event', {
+        "nodeid": nodeid,
+        "event": evtcode
+    });
+}
+
+//zwave controller notifications land here, timeouts, sleep notifications, etc.
+function notification(nodeid, notif, help) {
+    logging.log('notification: ' + util.format('node %d: %s', nodeid, help));
+    zwcallback('notification', {
+        nodeid: nodeid,
+        notification: notif,
+        help: help
+    });
+}
+
+//Like a 90's flatbed
+function scanComplete() {
+    logging.log('network scan complete.');
+    zwcallback('scan complete', {});
+}
+
+//Controller command responses, often from API calls
+function controllerCommand(nodeid, state, errcode, help) {
+    var obj = {
+        nodeid: nodeid,
+        state: state,
+        errcode: errcode,
+        help: help
+    };
+    logging.log('controllerCommannd: ' + util.format('command feedback received: %j', JSON.stringify(obj)));
+    zwcallback('controller command', obj);
+}
+
+//dispatch OpenZwave events to mqtt
+function zwcallback(event, arghash) {
+    logging.log('zwcallback: ' + util.format("%s, args: %j", event, arghash))
+    client.publish(zwaveTopic + '/' + event, JSON.stringify(arghash));
+}
+
